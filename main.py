8c88c7a4dea649774879c6d32fd3d1f581dfa5a1
cedrik8200 <cedrik8200@cedrik8200-Nitro-AN515-42>
2021-07-01 09:16:51 +0100

    ajout main.py et creation des modeles

---
+++ main.py
@@ -0,0 +1,12 @@
+from model import Player
+
+
+serialized_player = {
+    'Roux' : player.name,
+    '26/05/1978' : player.birth_date,
+    'Jean' : player.first_name,
+    'Homme' : player.sex,
+    '230' : player.ranking,
+}
+
+
---
+++ model.py
@@ -0,0 +1,24 @@
+from tinydb import TinyDB
+
+
+#
+class Player:
+    def __init__(self):
+        self.name = name
+        self.first_name = first_name
+        self.birth_date = birth_date
+        self.sex = sex
+        self.ranking = ranking
+
+
+#
+class Tournament:
+    def __init__(self):
+        pass
+
+
+#
+class PairManager:
+    def __init__(self):
+        pass
+
